79b81bd2cec73e72cafc0db1a5ea4dabd402f6bf
jon-kamis <jonmcmxcix@hotmail.com>
2024-01-03 14:46:36 -0500

    Feature: Enhanced Docker DB Testing platform

--- finance-manager-backend/cmd/finance-mngr/internal/testingutils/dockerdb.go
+++ finance-manager-backend/cmd/finance-mngr/internal/testingutils/dockerdb.go
@@ -1,11 +1,16 @@
 package testingutils
 
 import (
+	"database/sql"
 	"finance-manager-backend/cmd/finance-mngr/internal/fmlogger"
 	"finance-manager-backend/cmd/finance-mngr/internal/models"
 	"fmt"
+	"log"
+	"testing"
 	"time"
 
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,11 +20,19 @@ const db_username = "postgres"
 const db_password = "postgres"
 const db_name = "financemanager_test"
 
+var dockerConfig = GetDefaultConfig()
+
 type DockerDBConfig struct {
 	Postgres_version string
 	DB_username      string
 	DB_password      string
 	DB_name          string
+}
+
+type DockerTestPlatform struct {
+	DB       *sql.DB
+	Resource *dockertest.Resource
+	Pool     *dockertest.Pool
 }
 
 func GetDefaultConfig() DockerDBConfig {
@@ -93,6 +106,90 @@ func seedUserRoles(db *gorm.DB) {
 	fmlogger.Enter(method)
 
 	db.Create(&models.UserRole{ID: 1, UserId: 1, RoleId: 1, Code: "admin", CreateDt: time.Now(), LastUpdateDt: time.Now()})
+
+	fmlogger.Exit(method)
+}
+
+func Setup(m *testing.M) DockerTestPlatform {
+	method := "dockerdb.Setup"
+	fmlogger.Enter(method)
+
+	var err error
+	var pool *dockertest.Pool
+	var resource *dockertest.Resource
+	var testDB *sql.DB
+
+	options := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        dockerConfig.Postgres_version,
+		Env: []string{
+			"POSTGRES_USER=" + dockerConfig.DB_username,
+			"POSTGRES_PASSWORD=" + dockerConfig.DB_password,
+			"POSTGRES_DB=" + dockerConfig.DB_name,
+			"listen_addresses='*'",
+		},
+	}
+
+	//Create Docker Pool
+	pool, err = dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not construct docker pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not donnect to Docker: %s", err)
+	}
+
+	//Pulls the docker image and creates a container
+	resource, err = pool.RunWithOptions(
+		&options,
+		func(config *docker.HostConfig) {
+			config.AutoRemove = true
+			config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+		})
+
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf("host=localhost port=%s user=%s password=%s dbname=%s timezone=UTC connect_timeout=5", resource.GetPort("5432/tcp"), dockerConfig.DB_username, dockerConfig.DB_password, dockerConfig.DB_name))
+
+		if err != nil {
+			return err
+		}
+
+		return testDB.Ping()
+
+	}); err != nil {
+		log.Fatalf("Could not connect to database: %s", err)
+	}
+
+	fmlogger.Info(method, "DB Connection complete")
+
+	fmlogger.Info(method, "Initializing tables and seeding test data")
+	InitTables(resource.GetPort("5432/tcp"))
+	fmlogger.Info(method, "Table initialization and test data seeding complete")
+
+	plat := DockerTestPlatform{
+		DB:       testDB,
+		Resource: resource,
+		Pool:     pool,
+	}
+
+	fmlogger.Exit(method)
+	return plat
+}
+
+func TearDown(p DockerTestPlatform) {
+	method := "dockerdb.TearDown"
+	fmlogger.Enter(method)
+
+	if err := p.Pool.Purge(p.Resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
 
 	fmlogger.Exit(method)
 }
--- finance-manager-backend/cmd/finance-mngr/internal/validation/validation_test.go
+++ finance-manager-backend/cmd/finance-mngr/internal/validation/validation_test.go
@@ -1,95 +1,32 @@
 package validation
 
 import (
-	"database/sql"
 	"finance-manager-backend/cmd/finance-mngr/internal/fmlogger"
 	"finance-manager-backend/cmd/finance-mngr/internal/repository/dbrepo"
 	"finance-manager-backend/cmd/finance-mngr/internal/testingutils"
-	"fmt"
-	"log"
 	"os"
 	"testing"
 
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
 )
 
 var fmv FinanceManagerValidator
 
-var dockerConfig = testingutils.GetDefaultConfig()
-
 func TestMain(m *testing.M) {
-	method := "validation_test.go"
+	method := "validation_test.TestMain"
 	fmlogger.Enter(method)
 
-	var err error
-	var pool *dockertest.Pool
-	var resource *dockertest.Resource
-	var testDB *sql.DB
+	//Setup testing platform using docker
+	p := testingutils.Setup(m)
+	fmv = FinanceManagerValidator{DB: &dbrepo.PostgresDBRepo{DB: p.DB}}
 
-	options := dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        dockerConfig.Postgres_version,
-		Env: []string{
-			"POSTGRES_USER=" + dockerConfig.DB_username,
-			"POSTGRES_PASSWORD=" + dockerConfig.DB_password,
-			"POSTGRES_DB=" + dockerConfig.DB_name,
-			"listen_addresses='*'",
-		},
-	}
-
-	//Create Docker Pool
-	pool, err = dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("could not construct docker pool: %s", err)
-	}
-
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatalf("Could not donnect to Docker: %s", err)
-	}
-
-	//Pulls the docker image and creates a container
-	resource, err = pool.RunWithOptions(
-		&options,
-		func(config *docker.HostConfig) {
-			config.AutoRemove = true
-			config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-		})
-
-	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
-	}
-
-	if err := pool.Retry(func() error {
-		var err error
-		testDB, err = sql.Open("pgx", fmt.Sprintf("host=localhost port=%s user=%s password=%s dbname=%s timezone=UTC connect_timeout=5", resource.GetPort("5432/tcp"), dockerConfig.DB_username, dockerConfig.DB_password, dockerConfig.DB_name))
-
-		if err != nil {
-			return err
-		}
-
-		return testDB.Ping()
-
-	}); err != nil {
-		log.Fatalf("Could not connect to database: %s", err)
-	}
-
-	fmv = FinanceManagerValidator{DB: &dbrepo.PostgresDBRepo{DB: testDB}}
-	fmlogger.Info(method, "DB Connection complete")
-
-	fmlogger.Info(method, "Initializing tables and seeding test data")
-	testingutils.InitTables(resource.GetPort("5432/tcp"))
-	fmlogger.Info(method, "Table initialization and test data seeding complete")
-
+	//Execute Code
 	code := m.Run()
 
-	if err := pool.Purge(resource); err != nil {
-		log.Fatalf("Could not purge resource: %s", err)
-	}
+	//Tear down testing platform
+	testingutils.TearDown(p)
 
 	fmlogger.Exit(method)
 	os.Exit(code)
