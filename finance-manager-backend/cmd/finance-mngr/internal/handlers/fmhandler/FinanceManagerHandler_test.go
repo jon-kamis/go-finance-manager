4a72e129bf96bfda4dbd6272425deeae01846b44
jon-kamis <jonmcmxcix@hotmail.com>
2024-01-11 14:36:17 -0500

    Feature: Added tests for stocks APIs

--- finance-manager-backend/cmd/finance-mngr/internal/handlers/fmhandler/FinanceManagerHandler_test.go
+++ finance-manager-backend/cmd/finance-mngr/internal/handlers/fmhandler/FinanceManagerHandler_test.go
@@ -17,6 +17,7 @@ import (
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/stretchr/testify/assert"
 )
 
 var p testingutils.DockerTestPlatform
@@ -31,10 +32,11 @@ func TestMain(m *testing.M) {
 	p = testingutils.Setup(m)
 	db := &dbrepo.PostgresDBRepo{DB: p.DB}
 	fmh = FinanceManagerHandler{
-		DB:        db,
-		JSONUtil:  &jsonutils.JSONUtil{},
-		Validator: &validation.FinanceManagerValidator{DB: db},
-		Auth:      testingutils.GetTestAuth(),
+		DB:            db,
+		JSONUtil:      &jsonutils.JSONUtil{},
+		Validator:     &validation.FinanceManagerValidator{DB: db},
+		Auth:          testingutils.GetTestAuth(),
+		StocksEnabled: false,
 	}
 
 	//Set application's handler
@@ -63,4 +65,93 @@ func MakeRequest(method, url string, body interface{}, isAuthenticatedRequest bo
 	app.Routes().ServeHTTP(writer, request)
 	return writer
 
+}
+
+func ReadResponse(w *httptest.ResponseRecorder, j interface{}) error{
+	method := "FinanceManagerHandler_test.ReadResponse"
+	fmlogger.Enter(method)
+
+	err := json.NewDecoder(w.Body).Decode(&j)
+
+	if err != nil {
+		fmlogger.ExitError(method, "unexpected error", err)
+		return err
+	}
+
+	fmlogger.Exit(method)
+	return nil
+}
+
+func TestLoadApiKeyFromFile(t *testing.T) {
+	method := "FinanceManagerHandler_test.TestLoadApiKeyFromFile"
+	fmlogger.Enter(method)
+
+	//Write File to read from
+	pwd, _ := os.Getwd()
+	fileName := "TestLoadApiKeyFromFile.keytest"
+	content := "test content"
+
+	err := os.WriteFile(pwd+fileName, []byte(content), 0666)
+
+	if err != nil {
+		t.Errorf("failed to persist file prior to test")
+	}
+
+	fmCp := fmh
+	fmCp.StocksApiKeyFileName = fileName
+	fmCp.StocksEnabled = false
+	fmCp.PolygonApiKey = ""
+
+	//Run Test
+	err = fmCp.LoadApiKeyFromFile()
+	assert.Nil(t, err)
+	assert.True(t, fmCp.StocksEnabled)
+	assert.Equal(t, content, fmCp.PolygonApiKey)
+
+	//Run failing test
+	fmCp.StocksApiKeyFileName = "someotherfile.keytest"
+	err = fmCp.LoadApiKeyFromFile()
+	assert.NotNil(t, err)
+
+	//Clean up test file
+	err = os.Remove(pwd + fileName)
+
+	if err != nil {
+		t.Errorf("failed to clean up test files after test completion")
+	}
+
+	fmlogger.Exit(method)
+}
+
+func TestUpdateAndPersistAPIKey(t *testing.T) {
+	method := "FinanceManagerHandler_test.TestLoadApiKeyFromFile"
+	fmlogger.Enter(method)
+
+	pwd, _ := os.Getwd()
+	fileName := "TestUpdateAndPersistAPIKey.keytest"
+	content := "test content"
+
+	fmCp := fmh
+	fmCp.StocksApiKeyFileName = fileName
+	fmCp.StocksEnabled = false
+	fmCp.PolygonApiKey = ""
+
+	//Run Test
+	err := fmCp.UpdateAndPersistAPIKey(content)
+	assert.Nil(t, err)
+	assert.True(t, fmCp.StocksEnabled)
+	assert.Equal(t, content, fmCp.PolygonApiKey)
+
+	//Verify that test was successful
+	_, err = os.ReadFile(pwd + fileName)
+	assert.Nil(t, err)
+
+	//Clean up the test file
+	err = os.Remove(pwd + fileName)
+
+	if err != nil {
+		t.Errorf("failed to clean up test files after test completion")
+	}
+
+	fmlogger.Exit(method)
 }
